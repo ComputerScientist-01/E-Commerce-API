8d15f2b4c164c4cb8075b13d3e5088b08e02cbc1
Jayvardhan Rathi <39644109+ComputerScientist-01@users.noreply.github.com>
2021-03-05 12:34:29 +0530

    logging api requests"

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const app = express();
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
 
 //middleware
 app.use(bodyParser.json());
+app.use(morgan('tiny'));
 
 require("dotenv/config");
-
 const api = process.env.API_URL;
 
 app.get(`${api}/products`, (req, res) => {
