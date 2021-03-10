a69c3116126e9607459ae81c9d6d612dbf1bebc7
ComputerScientist-01 <rathi406@gmail.com>
2021-03-10 16:19:03 +0530

    read/write data via API

--- app.js
+++ app.js
@@ -14,22 +14,50 @@ app.use(
 );
 app.use(morgan("tiny"));
 
+//database schema
+const productSchema = mongoose.Schema({
+  name: String,
+  image: String,
+  countInStock: {
+    type: Number,
+    required: true,
+  },
+});
+
+// Models are fancy constructors compiled
+//from Schema definitions
+const Product = mongoose.model("Product", productSchema);
+
 require("dotenv/config");
 const api = process.env.API_URL;
 
-app.get(`${api}/products`, (req, res) => {
-  const product = {
-    id: 1,
-    name: "hair_dresser",
-    image: "some_url",
-  };
-  res.send(product);
+app.get(`${api}/products`, async (req, res) => {
+  const productList = await Product.find();
+  if (!productList) {
+    res.status(500).json({ success: false });
+  }
+
+  res.send(productList);
 });
 
 app.post(`${api}/products`, (req, res) => {
-  const newProduct = req.body;
-  console.log(newProduct);
-  res.send(newProduct);
+  const product = new Product({
+    name: req.body.name,
+    image: req.body.image,
+    countInStock: req.body.countInStock,
+  });
+
+  product
+    .save()
+    .then((createdProduct) => {
+      res.status(201).json(createdProduct);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        error: err,
+        success: false,
+      });
+    });
 });
 
 mongoose
