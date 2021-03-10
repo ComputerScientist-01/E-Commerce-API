5593d6cde06e6f5a3542fd86c3fa8c1e1d974175
ComputerScientist-01 <rathi406@gmail.com>
2021-03-10 11:46:45 +0530

    added connection to mongodb database and set up mongoose

--- app.js
+++ app.js
@@ -1,21 +1,28 @@
 const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
 const mongoose = require("mongoose");
+const options = require("dotenv/lib/env-options");
+
 //middleware
 app.use(bodyParser.json());
-app.use(morgan('tiny'));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(morgan("tiny"));
 
 require("dotenv/config");
 const api = process.env.API_URL;
 
 app.get(`${api}/products`, (req, res) => {
   const product = {
-    id:1,
-    name:'hair_dresser',
-    image: 'some_url',
-  }
+    id: 1,
+    name: "hair_dresser",
+    image: "some_url",
+  };
   res.send(product);
 });
 
@@ -24,6 +31,20 @@ app.post(`${api}/products`, (req, res) => {
   console.log(newProduct);
   res.send(newProduct);
 });
+
+mongoose
+  .connect(process.env.CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: "eshop-database",
+  })
+
+  .then(() => {
+    console.log("Database connection is ready....");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(3000, () => {
   console.log(api);
