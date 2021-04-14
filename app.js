63d9803d5767e355a51424da0d16cc58c409eced
ComputerScientist-01 <rathi406@gmail.com>
2021-04-14 20:04:57 +0530

    added cors

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv/config");
 
+//enabling cors
+app.use(cors());
+app.options("*", cors());
+
 //environment variables
 const api = process.env.API_URL;
 
