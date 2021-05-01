21a1ae5327c732fb16929159b621d29efe4bdf99
ComputerScientist-01 <rathi406@gmail.com>
2021-05-01 16:07:12 +0530

    completed category model

--- models/category.js
+++ models/category.js
@@ -1,7 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const categorySchema = mongoose.Schema({
+	name: {
+		type: String,
+		required: true,
+	},
+	icon: {
+		type: String,
+	},
+	color: {
+		type: String,
+	},
+});
 
-})
-
-exports.Category = mongoose.model('Category', categorySchema);
+exports.Category = mongoose.model("Category", categorySchema);
