c2ec4367f75d8de773a49b4526074df037bc735c
ComputerScientist-01 <rathi406@gmail.com>
2021-05-05 15:12:59 +0530

    addded user model

--- models/user.js
+++ models/user.js
@@ -1,11 +1,45 @@
 const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
-	name: String,
-	image: String,
-	countInStock: {
-		type: Number,
+	name: {
+		type: String,
 		required: true,
+	},
+	email: {
+		type: String,
+		required: true,
+	},
+	passwordHash: {
+		type: String,
+		required: true,
+	},
+	phone: {
+		type: String,
+		required: true,
+	},
+	isAdmin: {
+		type: Boolean,
+		default: false,
+	},
+	street: {
+		type: String,
+		default: "",
+	},
+	apartment: {
+		type: String,
+		default: "",
+	},
+	zip: {
+		type: String,
+		default: "",
+	},
+	city: {
+		type: String,
+		default: "",
+	},
+	country: {
+		type: String,
+		default: "",
 	},
 });
 
